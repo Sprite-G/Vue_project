268ebaa1ff17a94590c2af61d8028c251959ade6
Sprite-G <962112236@qq.com>
2019-10-31 21:53:51 +0800

    ssdfsdf

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 解决移动端300ms延迟问题，需要先安装fastClick
 // import './assets/styles/reset.css'
 import 'styles/reset.css'
@@ -11,9 +12,11 @@ import 'styles/reset.css'
 import 'styles/border.css'
 // import './assets/styles/iconfont.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -5,10 +5,12 @@
 </template>
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
